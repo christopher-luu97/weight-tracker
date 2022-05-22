6e5e4983959997a87a9c834d205eaedfe1bb6a2c
christopher-luu97 <christopher.luu1337@gmail.com>
2022-05-22 11:01:32 +1000

    Edit config file and import

--- db_connection.py
+++ db_connection.py
@@ -2,16 +2,16 @@ import pandas as pd
 import numpy as np
 import psycopg2 as ps2
 
-from config import *
+import config as cfg
 
 def connect():
     con = None
     try:
         con = ps2.connect(
-            host=host,
-            database=database,
-            user=user,
-            password=password
+            host=cfg["host"],
+            database=cfg["database"],
+            user=cfg["user"],
+            password=cfg["password"]
         )
 
         cur = con.cursor()
